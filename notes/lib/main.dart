6b43ec7d910ae8154b84249c49b2b2df36d80402
zTomz <tomvogel0@gmail.com>
2024-03-05 12:56:33 +0100

    Created basic notes app ui

--- notes/lib/main.dart
+++ notes/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Notes',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.black,
+          secondary: const Color(0xFF211E1C),
+          background: Colors.black,
+          brightness: Brightness.dark,
+        ),
+        iconButtonTheme: IconButtonThemeData(
+          style: IconButton.styleFrom(
+              backgroundColor: const Color(0xFF211E1C),
+              foregroundColor: const Color(0xFFFFFFFF),
+              iconSize: 45,
+              padding: const EdgeInsets.all(16)),
+        ),
+        textTheme: GoogleFonts.loraTextTheme(
+          const TextTheme(
+            headlineLarge: TextStyle(
+              fontSize: 70,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
         useMaterial3: true,
       ),
       home: const HomePage(),
